fc25d449411e3b1b00d9f330b102297c9a08dd78
Paweł Stiasny <pawelstiasny@gmail.com>
2014-05-19 01:25:13 +0200

    hyperbolic nav: plot hyperbola intersections

--- popty/06-hyperbolic/projekt.m
+++ popty/06-hyperbolic/projekt.m
@@ -14,6 +14,8 @@ circ_vec = @(X) [
     circ_c(X(1), X(2));
 ];
 
+hbplus = @(a, b, x0, y0, x) b .* sqrt ((x - x0) .^ 2 ./ a .^ 2 - 1) + y0;
+hbminus = @(a, b, x0, y0, x) -b .* sqrt ((x - x0) .^ 2 ./ a .^ 2 - 1) + y0;
 
 % obliczenia
 [opt, fval, info, output, fjac] = fsolve(circ_vec, [200; 200])
@@ -22,6 +24,13 @@ circ_vec = @(X) [
 % wykresy
 circle = @(centerX, centerY, radius) rectangle('Position',[centerX - radius, centerY - radius, radius*2, radius*2], 'Curvature',[1,1]);
 axis square;
+R = -4:0.1:4;
+hyperbola_ew = @(x0, y0, a, b, style) plot(...
+    -a*cosh(R) + x0, b*sinh(R) + y0, style,...
+    a*cosh(R)  + x0, b*sinh(R) + y0, style);
+hyperbola_ns = @(x0, y0, a, b, style) plot(...
+    b*sinh(R) + x0,  a*cosh(R) + y0, style,...
+    b*sinh(R) + x0, -a*cosh(R) + y0, style);
 
 plot(xa, 0, 'r*')
 hold on
@@ -37,5 +46,18 @@ circle(0,  0,  dist_c)
 
 plot(opt(1), opt(2), 'r*')
 text(opt(1), opt(2), 'OPT')
+
+for a = 1:2:20
+    b = sqrt((xa/2).^2 - a.^2);
+    hyperbola_ew(xa/2, 0, a, b, 'b');
+end
+a = abs(dist_a - dist_c) / 2
+b = sqrt((xa/2).^2 - a.^2)
+hyperbola_ew(xa/2, 0, a, b, 'r');
+
+a = abs(dist_b - dist_c) / 2
+b = sqrt((yb/2).^2 - a.^2)
+hyperbola_ns(0, yb/2, a, b, 'r');
+
 
 hold off
