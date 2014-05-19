bc70a7ebcb66b16d968b3f8effdab1b4cf5a5ecc
Paweł Stiasny <pawelstiasny@gmail.com>
2014-05-19 16:55:25 +0200

    hyperbolic nav: fsolve minimization

--- popty/06-hyperbolic/projekt.m
+++ popty/06-hyperbolic/projekt.m
@@ -5,25 +5,14 @@ dist_a = 3.188636072053379e+02;
 dist_b = 2.499479945908749e+02;
 dist_c = 3.368590209568388e+02;
 
-circ_a = @(x,y) (x-xa).^2 + y.^2 - dist_a.^2;
-circ_b = @(x,y) x.^2 + (y - yb).^2 - dist_b.^2;
-circ_c = @(x,y) x.^2 + y.^2 - dist_c.^2;
-circ_vec = @(X) [
-    circ_a(X(1), X(2));
-    circ_b(X(1), X(2));
-    circ_c(X(1), X(2));
-];
-
-hbplus = @(a, b, x0, y0, x) b .* sqrt ((x - x0) .^ 2 ./ a .^ 2 - 1) + y0;
-hbminus = @(a, b, x0, y0, x) -b .* sqrt ((x - x0) .^ 2 ./ a .^ 2 - 1) + y0;
-
-% obliczenia
-[opt, fval, info, output, fjac] = fsolve(circ_vec, [200; 200])
+opt = [143; 305];
 
 
 % wykresy
-circle = @(centerX, centerY, radius) rectangle('Position',[centerX - radius, centerY - radius, radius*2, radius*2], 'Curvature',[1,1]);
-axis square;
+circle = @(centerX, centerY, radius)...
+    rectangle('Position', [
+        centerX - radius, centerY - radius, radius*2, radius*2],...
+    'Curvature',[1,1]);
 R = -4:0.1:4;
 hyperbola_ew = @(x0, y0, a, b, style) plot(...
     -a*cosh(R) + x0, b*sinh(R) + y0, style,...
@@ -46,20 +35,34 @@ circle(0,  0,  dist_c)
 
 plot(opt(1), opt(2), 'r*')
 text(opt(1), opt(2), 'OPT')
+axis([-50 400 -50 400]);
 print -depsc circles.eps
 
 for a = 1:2:20
     b = sqrt((xa/2).^2 - a.^2);
     hyperbola_ew(xa/2, 0, a, b, 'b');
 end
-a = abs(dist_a - dist_c) / 2
-b = sqrt((xa/2).^2 - a.^2)
-hyperbola_ew(xa/2, 0, a, b, 'r');
+a_ew = abs(dist_a - dist_c) / 2
+b_ew = sqrt((xa/2).^2 - a_ew.^2)
+hyperbola_ew(xa/2, 0, a_ew, b_ew, 'r');
 print -depsc hyperbolas_ew.eps
 
-a = abs(dist_b - dist_c) / 2
-b = sqrt((yb/2).^2 - a.^2)
-hyperbola_ns(0, yb/2, a, b, 'r');
+a_ns = abs(dist_b - dist_c) / 2
+b_ns = sqrt((yb/2).^2 - a_ns.^2)
+hyperbola_ns(0, yb/2, a_ns, b_ns, 'g');
+print -depsc hyperbolas_ns.eps
+
+
+% obliczenia
+diary fvalopt.log
+circ_a = @(x,y) (x-xa).^2 + y.^2 - dist_a.^2;
+minfun = @(X) [
+    (X(1)-xa/2).^2 ./ a_ew.^2 - X(2).^2 ./ b_ew.^2 - 1;
+    (X(2)-yb/2).^2 ./ a_ns.^2 - X(1).^2 ./ b_ns.^2 - 1;
+    circ_a(X(1), X(2))
+];
+[opt, fval, ~, output, ~] = fsolve(minfun, [200; 200])
+diary off
 
 
 hold off
